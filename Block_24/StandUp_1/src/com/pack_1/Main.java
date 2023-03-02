a601a6eb39378281c6444a3dcf03722c1e77f076
aivnash-here <avinash.softer@gmail.com>
2023-03-02 23:52:29 +0530

    message

--- Block_24/StandUp_1/src/com/pack_1/Main.java
+++ Block_24/StandUp_1/src/com/pack_1/Main.java
@@ -1,11 +1,39 @@
 package com.pack_1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
-	
+		
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome!");
+		Set<Employee> set = new TreeSet<>();
+		
+		//Employee e = new Employee(1, "Rahul", "Delhi", 60000);
+		
+		set.add(new Employee(1, "Rahul", "Delhi", 60000));
+		set.add(new Employee(2, "Rahul", "Delhi", 70000));
+		set.add(new Employee(3, "Rahul", "Delhi", 40000));
+		
+//		set.add("Rahul");
+//		set.add("Avinash");
+//		set.add("Rahul");
+		
+		
+		for(Employee e : set)
+		System.out.println(e);
+		
+//		for(String e : set)
+//			System.out.println(e);
+		
 		
 	}
-
+	
 }
+
+
+
